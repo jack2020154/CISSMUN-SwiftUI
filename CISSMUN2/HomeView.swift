42ac3592ab09da674fedbdd910bee691c3e1f204
Jack Wang <jack2020154@concordiashanghai.org>
2020-01-19 13:32:23 +0800

    PDFViewController template built, basic home screen implemented

--- CISSMUN2/HomeView.swift
+++ CISSMUN2/HomeView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View Template")
+        ScrollView {
+        VStack{
+            //MARK: Main Vertical Stack
+            Text("Welcome").multilineTextAlignment(.center)
+            Spacer().frame(height: 50)
+            HStack {
+                //MARK: Main Block Stack
+                VStack{
+                    HStack {
+                        VStack (alignment: .leading){
+                            Text("CISSMUN Director").padding(.horizontal)
+                            Text("Erik Paulson").padding(.horizontal)
+                        }
+                        Spacer()
+                        Text("Image of paulson")
+                    }
+                    Spacer().frame(height: 10)
+                    Text("It is both an honor and a privilege to welcome you to the eleventh annual session of Concordia International School Shanghai Model United Nations. Whatever role you will play in this year’s event, whether chair, judge, delegate, member of the press team or anything else, I hope that the conference will leave you with a heightened understanding of the social and political challenges confronting the international community, and that you will come away more practiced in the arts of public speaking, negotiation and compromise. I look forward to your active participation in the eleventh annual session of CISSMUN, and I wish you every success.").multilineTextAlignment(.leading).padding(.horizontal)
+                }
+            }.cornerRadius(10).overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black)).padding([.top, .horizontal])
+            HStack {
+                //MARK: Main Block Stack
+                VStack{
+                    HStack {
+                        VStack (alignment: .leading){
+                            Text("CISSMUN Director")
+                            Text("Erik Paulson")
+                        }
+                        Spacer()
+                        Text("Image of paulson")
+                    }
+                    Spacer().frame(height: 10)
+                    Text("It is both an honor and a privilege to welcome you to the eleventh annual session of Concordia International School Shanghai Model United Nations. Whatever role you will play in this year’s event, whether chair, judge, delegate, member of the press team or anything else, I hope that the conference will leave you with a heightened understanding of the social and political challenges confronting the international community, and that you will come away more practiced in the arts of public speaking, negotiation and compromise. I look forward to your active participation in the eleventh annual session of CISSMUN, and I wish you every success.").multilineTextAlignment(.leading).padding(.horizontal)
+                }
+            }.cornerRadius(10).overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black)).padding([.top, .horizontal])
+        }
+        }
     }
 }
 
--- CISSMUN2/ProcedureView.swift
+++ CISSMUN2/ProcedureView.swift
@@ -8,9 +8,32 @@
 
 import SwiftUI
 
+struct Files: Identifiable {
+    var id: Int
+    var displayname: String
+    var filename: String
+}
+
+
 struct ProcedureView: View {
+
+    let filelist: [Files] = [
+        Files(id: 1, displayname: "Cover Page", filename: "coverpage.pdf"),
+        Files(id: 2, displayname: "Preparatory Meeting Guidelines", filename: "prepmeetguides.pdf"),
+        Files(id: 3, displayname: "CISSMUN Rules of Procedure", filename: "cissrop.pdf"),
+        Files(id: 4, displayname: "Chairing at CISSMUN", filename: "munchair.pdf"),
+        Files(id: 5, displayname: "Appendix 1: THIMUN Rules of Procedure", filename: "thirop.pdf"),
+        Files(id: 6, displayname: "Appendix 2: United Nations Charter", filename: "uncharter.pdf")
+    ]
+    
     var body: some View {
-        Text("Procedure View Template")
+        NavigationView {
+            List(filelist, id:\.id) { file in
+                NavigationLink(destination: PDFViewController(dname: file.displayname, fname: file.filename)) {
+                    Text(file.displayname)
+                }
+            }.navigationBarTitle(Text("Documents"))
+        }
     }
 }
 
